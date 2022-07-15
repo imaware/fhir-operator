834dc047f42bb3db06b20242f505f2c502ccdaad
elebioda <ethan@imaware.health>
2022-07-15 11:41:42 -0500

    fix: check if fhir resource should be retried lazily

--- api/fhirresource_operations_test.go
+++ api/fhirresource_operations_test.go
@@ -56,3 +56,37 @@ func Test_create_or_update_failed_fhir_resource(t *testing.T) {
 		t.Errorf("expected boolean %t, got %t", expected, enqueu)
 	}
 }
+
+func Test_fhir_resrouce_is_to_be_updated(t *testing.T) {
+	var fhirResource = &v1alpha1.FhirResource{}
+	var expected = true
+	toBeUpdated := IsFhirResourceToBeUpdatedOrCreated(fhirResource)
+	if expected != toBeUpdated {
+		t.Errorf("expected boolean %t, got %t", expected, toBeUpdated)
+	}
+
+}
+
+func Test_fhir_resrouce_is_to_be_updated_last_applied_annotation(t *testing.T) {
+	var fhirResource = &v1alpha1.FhirResource{}
+	fhirResource.Annotations = map[string]string{
+		"kubectl.kubernetes.io/last-applied-configuration": "HELLO",
+	}
+	var expected = true
+	toBeUpdated := IsFhirResourceToBeUpdatedOrCreated(fhirResource)
+	if expected != toBeUpdated {
+		t.Errorf("expected boolean %t, got %t", expected, toBeUpdated)
+	}
+
+}
+
+func Test_fhir_resrouce_is_to_not_be_updated_created_status(t *testing.T) {
+	var fhirResource = &v1alpha1.FhirResource{}
+	fhirResource.Status.Status = CREATED
+	var expected = false
+	toBeUpdated := IsFhirResourceToBeUpdatedOrCreated(fhirResource)
+	if expected != toBeUpdated {
+		t.Errorf("expected boolean %t, got %t", expected, toBeUpdated)
+	}
+
+}
